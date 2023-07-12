69e33eb4d59dc9f8b1db2d49237f9db33336cdba
Razo <bestwishtous@gmail.com>
2023-07-12 19:14:48 +0800

    feature: wrangler.toml动态生成

--- wrangler.js
+++ wrangler.js
@@ -1,21 +1,26 @@
 const fs = require('fs');
 const TOML = require('@ltd/j-toml');
 const { merge } = require('lodash');
-fs.readFile('./wrangler.toml', (err, content) => {
-	if (!err) {
-		const tomlData = TOML.parse(content);
-		const { UUID } = process.env;
-		if (UUID) {
-			merge(tomlData, {
-				vars: {
-					UUID,
-				},
-			});
-		}
-		fs.writeFile('./wrangler.toml', TOML.stringify(tomlData).join('\n'), () => {
-			/**
-			 *
-			 */
-		});
-	}
+const wranglerToml = `
+name = 'vlessnode'
+main = 'src/worker.js'
+compatibility_date = '2023-06-27'
+
+[vars]
+
+UUID = '0b87fff8-ed16-4269-b53b-4aa9a01682a3'
+`;
+const tomlData = TOML.parse(wranglerToml);
+const { UUID } = process.env;
+if (UUID) {
+	merge(tomlData, {
+		vars: {
+			UUID,
+		},
+	});
+}
+fs.writeFile('./wrangler.toml', TOML.stringify(tomlData).join('\n'), () => {
+	/**
+	 *
+	 */
 });
